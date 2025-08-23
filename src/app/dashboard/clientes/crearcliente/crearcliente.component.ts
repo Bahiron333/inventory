1eb387f3d9018365f3dec50ba5f85be83334e723
Bahiron333 <bahiron39macro@gmail.com>
2025-08-23 12:00:01 -0500

    update dasboard

---
+++ src/app/dashboard/clientes/crearcliente/crearcliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crearcliente',
+  standalone: false,
+  templateUrl: './crearcliente.component.html',
+  styleUrl: './crearcliente.component.scss'
+})
+export class CrearclienteComponent {
+
+}
